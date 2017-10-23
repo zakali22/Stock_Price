6672f372d4e6fe5573a8d7ff628ac8e73edcd12d
Zakeria Ali <zak.fined@gmail.com>
2017-10-23 23:29:58 +0100

    Create a custom url based on stock name

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const https = require('https');
 // Storing API key in api.json
 const api = require('./api.json');
+
+// Run a function for custom stock search
+const stockPrice = (stock) => {
+  // Create 
+  const customUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock}&interval=5min&apikey=${api.key}`;
+
+};
