fc7bb9f0d5081fa3a10bdb25131305114e53f46b
Zakeria Ali <zak.fined@gmail.com>
2017-10-23 23:46:32 +0100

    Make request and grab latest piece of info

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ const api = require('./api.json');
 
 // Run a function for custom stock search
 const stockPrice = (stock) => {
-  // Create 
+  // Create a variable for each stock url
   const customUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stock}&interval=5min&apikey=${api.key}`;
+  // Do a https request and recieve data in return
+  https.get(customUrl, (response) => {
+    // Concatenate the full body of the response
+    let fullBody = '';
+    response.on('data', (data) => {
+      fullBody += data.toString();
+    }).on('end', () => {
+      // Parse the string response to a JSON object
+      const parsed = JSON.parse(fullBody);
+      // Grab the specific object property that contains all the price details
+      const arrayOfSeries = parsed['Time Series (5min)'];
+      let arr = [];
+      // Push all the objects into an array
+      for(let element in arrayOfSeries){
+        arr.push(arrayOfSeries[element]);
+      }
+      // Log the final data to the console
+      console.log(arr[arr.length - 1]);
 
+    });
+  });
 };
